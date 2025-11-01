26199d8cdec63717eff9b89d2fb81f2258bcd18d
MSM2002 <manaswi.m02@gmail.com>
2025-11-01 11:59:22 +0530

    💡 sentence_transformers minimal stub

---
+++ typings/sentence_transformers/__init__.pyi
@@ -0,0 +1,16 @@
+from typing import List
+import numpy as np
+from numpy.typing import NDArray
+
+class SentenceTransformer:
+    def __init__(self, model_name_or_path: str) -> None: 
+        ...
+    
+    def encode(
+        self,
+        sentences: List[str],
+        batch_size: int = 32,
+        show_progress_bar: bool = False,
+        convert_to_numpy: bool = True,
+    ) -> NDArray[np.float64]:
+        ...
