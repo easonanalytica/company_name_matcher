e42758619bdcee9d04a41a181714ed2391473c0a
MSM2002 <manaswi.m02@gmail.com>
2025-11-01 13:18:12 +0530

    🐛 precise types in joblib stub with TypedDict

--- typings/joblib/__init__.pyi
+++ typings/joblib/__init__.pyi
@@ -1,8 +1,19 @@
-from typing import Union, Dict, Optional
+from typing import Union, Dict, Optional, TypedDict
 import os
 import numpy as np
 from numpy.typing import NDArray
-from sklearn.cluster import KMeans
+
+class KMeans():
+    def __init__(self, n_clusters:int) -> None:
+         ...
+
+    def fit_predict(self, X: NDArray[np.floating]) -> NDArray[np.signedinteger]: 
+        ...
+
+
+class LoadData(TypedDict):
+    kmeans: KMeans
+    clusters: NDArray[np.signedinteger]
 
 def dump(
         value: Dict[str, Optional[Union[KMeans, NDArray[np.signedinteger]]]],
@@ -12,5 +23,5 @@ def dump(
 
 def load(
         filename: Union[str, os.PathLike[str]]
-) -> Dict[str, Union[KMeans, NDArray[np.signedinteger]]]:
+) -> LoadData:
     ...
