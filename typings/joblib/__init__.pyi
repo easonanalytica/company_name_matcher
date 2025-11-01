b6d5c73126a606be93b64ab7fe1355ea622b71e5
MSM2002 <manaswi.m02@gmail.com>
2025-11-01 13:23:31 +0530

    🐛 multiple KMeans types to a single definition

--- typings/joblib/__init__.pyi
+++ typings/joblib/__init__.pyi
@@ -2,21 +2,15 @@ from typing import Union, Dict, Optional, TypedDict
 import os
 import numpy as np
 from numpy.typing import NDArray
-
-class KMeans():
-    def __init__(self, n_clusters:int) -> None:
-         ...
-
-    def fit_predict(self, X: NDArray[np.floating]) -> NDArray[np.signedinteger]: 
-        ...
+from sklearn.cluster import KMeans as SKLearnKMeans
 
 
 class LoadData(TypedDict):
-    kmeans: KMeans
+    kmeans: SKLearnKMeans
     clusters: NDArray[np.signedinteger]
 
 def dump(
-        value: Dict[str, Optional[Union[KMeans, NDArray[np.signedinteger]]]],
+        value: Dict[str, Optional[Union[SKLearnKMeans, NDArray[np.signedinteger]]]],
         filename: Union[str, os.PathLike[str]]
 ) -> None:
     ...
